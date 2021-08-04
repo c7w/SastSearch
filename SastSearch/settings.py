b455549d2d27f8baac5b92bf5edca7f855eb3289
c7w <admin@cc7w.cf>
2021-08-04 21:33:50 +0800

    disable DEBUG; generate requirements.txt

--- SastSearch/settings.py
+++ SastSearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6$*l*u7+m^0ak^qty6se#!)(%%6hhrxy7@*1b@v-ekaqvu5z%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
